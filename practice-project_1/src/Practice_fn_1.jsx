4816f2f768718d24ddd92fd36da9d84718ebedc0
Nabh Garg <nabhgargdholpur@gmail.com>
2025-08-12 01:02:49 +0530

    make my coustom component learn new concepts of react with it.

---
+++ practice-project_1/src/Practice_fn_1.jsx
@@ -0,0 +1,7 @@
+function Chai () {
+    return (
+        <h1> Just For practice </h1>
+    )
+}
+
+export default Chai;
--- practice-project_1/src/main.jsx
+++ practice-project_1/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Chai from './Practice_fn_1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Chai />
   </StrictMode>,
 )
